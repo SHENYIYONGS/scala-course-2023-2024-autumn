25ec608bfbc158801405c38f4327617e98e958ce
SHENYIYONGS <150781300+SHENYIYONGS@users.noreply.github.com>
2024-03-22 12:54:07 +0800

    week-2-2
    week-2-2

--- src/main/scala/karazin/scala/users/group/week2/homework/Homework.scala
+++ src/main/scala/karazin/scala/users/group/week2/homework/Homework.scala
@@ -5,9 +5,7 @@ import scala.math.{abs, signum}
 
 object Homework:
 
-  // `x` and `y` are inaccessible from outside
   class Rational(x: Int, y: Int):
-    // Checking the precondition. Is fails then throws `IllegalArgumentException`
     require(y > 0, "Denominator must be positive")
 
     def this(x: Int) = this(x, 1)
@@ -15,56 +13,25 @@ object Homework:
     val numer = x / g
     val denom = y / g
 
-    // Defines an external name for a definition
-    @targetName("less than")
-    // Annotation on a method definition allows using the method as an infix operation
-    infix def <(that: Rational): Boolean =
-      this.numer * that.denom < that.numer * this.denom
-
-    @targetName("less or equal")
-    infix def <=(that: Rational): Boolean =
-      this < that || this == that
-
-    @targetName("greater than")
-    infix def >(that: Rational): Boolean =
-      !(this <= that)
-
-    @targetName("greater or equal")
-    infix def >=(that: Rational): Boolean =
-      !(this < that)
-
     @targetName("addition")
-    infix def +(that: Rational): Rational = {
-      val newNumer = this.numer * that.denom + that.numer * this.denom
-      val newDenom = this.denom * that.denom
-      Rational(newNumer, newDenom)
-    }
-
+    infix def +(that: Rational): Rational =
+      new Rational(this.numer * that.denom + that.numer * this.denom, this.denom * that.denom)
 
     @targetName("negation")
-    infix def unary_- : Rational = Rational(-numer, denom)
-
-    // Subtraction with recursion
-
+    infix def unary_- : Rational =
+      new Rational(-this.numer, this.denom)
 
     @targetName("substraction")
-    infix def -(that: Rational): Rational = this + (-that)
-
-    // Multiplication with recursion
-
+    infix def -(that: Rational): Rational =
+      this + (-that)
 
     @targetName("multiplication")
     infix def *(that: Rational): Rational =
-      Rational(this.numer * that.numer, this.denom * that.denom)
-
-    // Division with recursion
-
+      new Rational(this.numer * that.numer, this.denom * that.denom)
 
     @targetName("devision")
     infix def /(that: Rational): Rational =
-      Rational(this.numer * that.denom, this.denom * that.numer)
-
-
+      new Rational(this.numer * that.denom, this.denom * that.numer)
 
     override def toString: String = s"${this.numer}/${this.denom}"
 
@@ -73,15 +40,9 @@ object Homework:
 
     private lazy val g = gcd(abs(x), y)
 
-    override def equals(other: Any): Boolean =  {
-      other match {
-        case that: Rational =>
-          numer == that.numer && denom == that.denom
-        case _ => false
-      }
-    }
-
-
+    override def equals(other: Any): Boolean = other match
+      case that: Rational => this.numer == that.numer && this.denom == that.denom
+      case _ => false
 
   end Rational
 
