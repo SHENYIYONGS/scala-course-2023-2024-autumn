0cc556550c655ad826003508500e5dacf5f6737e
SHENYIYONGS <150781300+SHENYIYONGS@users.noreply.github.com>
2024-03-22 13:58:10 +0800

    week-3-3
    week-3-3

--- src/main/scala/karazin/scala/users/group/week3/Homework.scala
+++ src/main/scala/karazin/scala/users/group/week3/Homework.scala
@@ -1,69 +1,54 @@
 package karazin.scala.users.group.week3
 
 object Homework:
-  
+
   // Peano numbers
   abstract class Nat:
     def isZero: Boolean
     def predecessor: Nat
-    def successor: Nat = Succ(this)
-    
+    def successor: Nat = new Succ(this)
+
     infix def + (that: Nat): Nat
-    
+
     infix def - (that: Nat): Nat
-    
+
     // Optional task
     def toInt: Int
-    
-    // Optional task
-    def fromInt(int: Int) =
-      if (int <= 0) Zero
-    else Succ(fromInt(int - 1))
 
+    def fromInt(int: Int): Nat =
+      if int == 0 then Zero else new Succ(fromInt(int - 1))
 
+    override def toString: String = s"Nat(${this.toInt})"
 
-    override def toString: String = s"Nat($predecessor)"
-  
-  type Zero = Zero.type 
+  type Zero = Zero.type
   object Zero extends Nat:
     def isZero: Boolean = true
     def predecessor: Nat = throw new Exception("0 doesn't have a predecessor")
-    
+
     infix def +(that: Nat): Nat = that
-    
+
     infix def -(that: Nat): Nat =
-      if (that.isZero) this
-      else throw new Exception("Negative number result")
-
-
+      if that.isZero then this else throw new Exception("Negative number")
 
     // Optional task
     def toInt: Int = 0
 
-    override def toString: String = "Zero"
-    override def equals(obj: Any): Boolean =
-      obj.isInstanceOf[Zero.type] // Zero is equal to Zero
-
-
+    override def equals(obj: Any): Boolean = obj match
+      case _: Zero.type => true
+      case _ => false
 
   class Succ(n: Nat) extends Nat:
     def isZero: Boolean = false
     def predecessor: Nat = n
-    
-    infix def +(that: Nat): Nat =  successor + that
-    
+
+    infix def +(that: Nat): Nat = new Succ(n + that)
+
     infix def -(that: Nat): Nat =
-      if (that.isZero) this
-      else predecessor - that.predecessor
-
-
+      if that.isZero then this else n - that.predecessor
 
     // Optional task
-    def toInt: Int = 1 + predecessor.toInt
+    def toInt: Int = 1 + n.toInt
 
-    override def equals(obj: Any): Boolean = obj match {
-      case s: Succ => s.predecessor == this.predecessor
+    override def equals(obj: Any): Boolean = obj match
+      case succ: Succ => succ.toInt == this.toInt
       case _ => false
-    }
-
-
